282836a9cd5c9cae06ce6a4a7a0dbd99bb1c5a61
Thaileaf <lethaison2019@gmail.com>
2022-11-07 22:55:43 -0500

    finished portfolio website

---
+++ Portfolio/projects.js
@@ -0,0 +1,57 @@
+var projects = [
+    {
+        title:"Web Development Playground",
+        description:"Series of web pages developed in the pursuit of html/css/javascript knowledge.",
+        src:"./src/webdev.gif",
+        link:"../index.html",
+        tags: ["javascript", "html", "css"]
+    },{
+        title:"Perlin Noise Flow Field Simulator",
+        description:"An interactive canvas using P5.js to simulate particle physics through a perlin noise flow field.",
+        src:"./src/flowfield.gif",
+        link:"../P5/index.html",
+        tags:["processing", "html", "css", "javascript"]
+    },{
+        title:"NYU Augmented Library Mobile App",
+        description:"A mobile application designed at NYU with the aim of providing NYU Dibner library users ease of access to all services through the use of AR technology.",
+        src:"./src/vip.png",
+        link:"https://engineering.nyu.edu/research-innovation/student-research/vertically-integrated-projects/vip-teams/augmented-library",
+        tags:["mysql", "css", "javascript", "react", "react native", "python", "django"]
+    },{
+        title:"E20 Assembler",
+        description:"An assembler that parses instructions from a rudimentary assembly language, transposing it into machine code",
+        src:"./src/asm.png",
+        link:"https://github.com/Thaileaf/E20CompilerSimulator",
+        tags:["c++", "assembly"]
+    },{
+        title:"E20 Processor Simulator",
+        description:"A simulator to simulate a processor that is designed off a rudimentary instruction set architecture, based off of the x86 processor.",
+        src:"./src/sim.png",
+        link:"https://github.com/Thaileaf/E20CompilerSimulator",
+        tags:["c++", "assembly"]
+    },{
+        title:"Discord Bot Animal Racer",
+        description:"A small discord bot using the Discord API to create a small racing game with animal emojis between multiple users.",
+        src:"",
+        link:"https://github.com/Thaileaf/DiscordProjectFirst/blob/master/cogs/AnimalRace.py",
+        tags:["python"]
+    },{
+        title:"Bash and Powershell Scripts",
+        description:"A handful of bash and powershell scripts that provide use in work and projects",
+        src:"./src/bash.png",
+        link:"https://github.com/Thaileaf/scripts",
+        tags:["bash"]
+    },{
+        title:"Spotify Manager",
+        description:"A desktop application that allows Spotify users to create back ups of playlist and song information through the spotify API.",
+        src:"",
+        link:"https://github.com/Thaileaf/SpotifyManager",
+        tags:["python"]
+    },{
+        title:"Python Hot Reloading",
+        description:"A python module designed to hot reload python files, similar to javascript hot reloading. This allows changes to be made the program while it is running",
+        src:"./src/hot-reload.png",
+        link:"https://github.com/Thaileaf/Python-Hot-Reloader",
+        tags:["python"]
+    }
+]
